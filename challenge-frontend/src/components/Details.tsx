bcf940f49a9030cce5f7a317fd4c1b8ee7865730
Franco Buceta <francobuceta95@gmail.com>
2025-02-22 13:58:01 -0300

    feat: add new detail & button

--- challenge-frontend/src/components/Details.tsx
+++ challenge-frontend/src/components/Details.tsx
@@ -1,12 +1,11 @@
 import { FC } from "react";
+import Button from "./ui/Button";
 
 interface DetailsProps {
   data: Pokemon;
 }
 
 const Details: FC<DetailsProps> = ({ data }) => {
-  console.log(data);
-  
   return (
     <section className="min-h-[600px] flex flex-col justify-center p-10 bg-white rounded-md">
       <div className="w-full flex flex-col md:flex-row justify-center gap-10 md:gap-20">
@@ -19,19 +18,38 @@ const Details: FC<DetailsProps> = ({ data }) => {
         </div>
         <div>
           <ul className="flex flex-col gap-3 text-2xl">
-            <li>Height: <span className="text-gray-600">{data.height}</span></li>
-            <li>Base Experience: <span className="text-gray-600">{data.base_experience}</span></li>
+            <li>
+              Height: <span className="text-gray-600">{data.height}</span>
+            </li>
+            <li>
+              Base Experience:{" "}
+              <span className="text-gray-600">{data.base_experience}</span>
+            </li>
             <div>
               <span>Abilities:</span>
               <div className="flex flex-col pl-4">
-                {
-                  data.abilities.map(elem => (
-                    <span key={elem.slot} className="text-gray-600 uppercase">{elem.ability.name}</span>
-                  ))
-                }
+                {data.abilities.map((elem) => (
+                  <span key={elem.slot} className="text-gray-600 uppercase">
+                    {elem.ability.name}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div>
+              <span>Types:</span>
+              <div className="flex flex-col pl-4">
+                {data.types.map((elem) => (
+                  <span key={elem.slot} className="text-gray-600 uppercase">
+                    {elem.type.name}
+                  </span>
+                ))}
               </div>
             </div>
           </ul>
+
+          <div className="pt-10">
+            <Button content="Agregar a favoritos" />
+          </div>
         </div>
       </div>
     </section>
